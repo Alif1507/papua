55debd0692a4ae6ac82b93ab2176613adcf42747
Alif1507 <m.alifwahyudi2007@gmail.com>
2024-10-13 11:46:44 +0700

    added loading

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.addEventListener('load', function() {
       document.getElementById('loading').style.opacity = '0';
       document.getElementById('loading').style.visibility = 'hidden';
 
-  }, 4000); 
+  }, 6000); 
 });
 
 
